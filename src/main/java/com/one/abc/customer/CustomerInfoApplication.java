e2b017fba95e4501a1099fc2392fb49b5458c558
patil301 <patil301@avaya.com>
2023-06-28 16:45:45 +0530

    Changes for deployment

--- src/main/java/com/one/abc/customer/CustomerInfoApplication.java
+++ src/main/java/com/one/abc/customer/CustomerInfoApplication.java
@@ -2,9 +2,16 @@ package com.one.abc.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CustomerInfoApplication {
+public class CustomerInfoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CustomerInfoApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerInfoApplication.class, args);
