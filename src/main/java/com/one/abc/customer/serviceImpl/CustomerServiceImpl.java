a3703e0299aa2b98be90341bbba83e9842132065
patil301 <patil301@avaya.com>
2023-06-30 19:27:57 +0530

    Added validation

--- src/main/java/com/one/abc/customer/serviceImpl/CustomerServiceImpl.java
+++ src/main/java/com/one/abc/customer/serviceImpl/CustomerServiceImpl.java
@@ -1,6 +1,8 @@
 package com.one.abc.customer.serviceImpl;
 
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,16 +34,19 @@ public class CustomerServiceImpl implements CustomerService {
 
 	@Override
 	public Customer getCustomer(Long customerId, String mobileNumber) {
-			if (customerId == null && Strings.isBlank(mobileNumber)) {
-				throw new CustomException(1101, "Please provide customerId or Mobile number");
-			} else if (customerId != null) {
-				Optional<Customer> customer = customerRepo.findById(customerId);
-				return getCustomerFromDb(customer);
-			} else if (!Strings.isBlank(mobileNumber)) {
+		if (customerId == null && Strings.isBlank(mobileNumber)) {
+			throw new CustomException(1101, "Please provide customerId or Mobile number");
+		} else if (customerId != null) {
+			Optional<Customer> customer = customerRepo.findById(customerId);
+			return getCustomerFromDb(customer);
+		} else {
+			if (!Strings.isBlank(mobileNumber) && isValidMobileNumber(mobileNumber)) {
 				Optional<Customer> customer = customerRepo.findByMobileNumber(mobileNumber);
 				return getCustomerFromDb(customer);
+			} else {
+				throw new CustomException(1101, "Please provide valid Mobile number");
 			}
-		return null;
+		}
 	}
 
 	private Customer getCustomerFromDb(Optional<Customer> customer) {
@@ -49,6 +54,16 @@ public class CustomerServiceImpl implements CustomerService {
 			return customer.get();
 		} else {
 			throw new CustomException(1100, "Customer does not exist");
+		}
+	}
+	
+	private boolean isValidMobileNumber(String mobileNumber) {
+		if (Strings.isNotEmpty(mobileNumber)) {
+			Pattern p = Pattern.compile("^\\d{10}$");
+			Matcher m = p.matcher(mobileNumber);
+			return m.matches();
+		} else {
+			return false;
 		}
 	}
 }
