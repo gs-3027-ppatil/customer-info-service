65cca5c0a60becd5022a0deaebe5837264977df8
gs-3773-niteen <niteen.singh@gslab.com>
2023-07-05 10:38:41 +0530

    commit: PinManagement

--- src/main/java/com/one/abc/customer/entity/PinMgt.java
+++ src/main/java/com/one/abc/customer/entity/PinMgt.java
@@ -39,7 +39,7 @@ public class PinMgt {
 	private String modifiedBy;
 	
 	@Column(name = "Active")
-	private boolean active;
+	private String active;
 
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "fk_cust_id")
